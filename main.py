6e46b83a32938a539a4f6192308d6ec2dabe6bb9
Mike Jones <mikejones744@icloud.com>
2025-10-04 13:22:54 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    print("Hello from exec1!")
+"""
+Exercise 1.
+
+4 Oct 2025
+"""
+
+
+def clear_screen() -> None:
+    """Clear screen method."""
+    print("\033c", end="")
+
+
+def main() -> None:
+    clear_screen()
+
+    print("Hi Exercise 1")
 
 
 if __name__ == "__main__":
